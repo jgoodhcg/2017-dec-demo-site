64282873c87a7a766f99d314687818b66a167904
jgoodhcg <jgoodhcg@gmail.com>
2018-01-06 21:02:54 -0500

    Add desktop home page stubs

--- app/src/cljs/app/pages/home.cljs
+++ app/src/cljs/app/pages/home.cljs
@@ -11,8 +11,17 @@
 
 (defn home-page []
 
-  [:div {:style {:display "flex" :flex-wrap "wrap" :align-content "stretch"}}
-   [ui/paper
-    [:div [:p "this is some picture"]]]
-   [ui/paper
-    [:div [:p "this is some bio this content has a bunch more stuff"]]]])
+  [:div {:style {:display "flex" :flex-wrap "nowrap"
+                 :justify-content "center"}}
+
+   [:div {:style {:display "flex" :flex "0 1 40%"}}
+    [ui/card
+     [ui/card-media [:img {:src
+                           ;; (str js/context "/img/file-name-in-resources-public-img.jpg")
+                           "https://dummyimage.com/1600x900/1568c0/ffffff.jpg&text=A+nice+picture+of+me"
+                           :alt "a decent picture of myself"}]]]]
+
+   [:div {:style {:display "flex" :flex "0 0 auto"}}
+    [ui/paper {:style {:padding "1em"}}
+     [:div [:p "this is some bio this content has a bunch more stuff"]]]]
+   ])
